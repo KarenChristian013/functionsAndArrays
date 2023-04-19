5205446cc7f742f17f3d038aa7f366c19dd300e4
KarenChristian <karen.lewis013@yahoo.com>
2023-04-18 21:23:04 -0400

    soConfusedOne

---
+++ app.js
@@ -0,0 +1,25 @@
+var a = 1
+var b = 2
+function sum (a,b){
+    return a+b;
+};
+var result= sum(a,b);
+console.log(result)
+
+function difference (a,b){
+    return a-b;
+};
+var result = difference (a,b);
+console.log(result)
+
+function multiplied (a,b){
+    return a*b;
+};
+var result=multiplied (a,b);
+console.log(result)
+
+function division (a,b){
+    return a/b;
+};
+var result=division (a,b)
+console.log(result) 
